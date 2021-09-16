039bcd0b3b02dd2bdee68d0e4269da74db0f4704
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-09-16 10:00:56 +0700

    Update empty.c

---
+++ src/glx/empty.c
@@ -0,0 +1,22 @@
+#include <GL/gl.h>
+
+GLboolean
+glAreTexturesResidentEXT(GLsizei n, const GLuint * textures,
+                         GLboolean * residences)
+{
+    return glAreTexturesResident(n, textures, residences);
+}
+void glDeleteTexturesEXT(GLsizei n, const GLuint *textures)
+{
+    glDeleteTextures(n, textures);
+}
+
+void glGenTexturesEXT(GLsizei n, GLuint *textures)
+{
+    glGenTextures(n, textures);
+}
+
+GLboolean glIsTextureEXT(GLuint texture)
+{
+    return glIsTexture(texture);
+}
